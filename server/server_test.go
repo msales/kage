87afd0e3735e98aceef2dc7de4e8a9809f495519
Nicholas Wiersma <nick@wiersma.co.za>
2017-03-13 15:47:56 +0100

    Added server tests

---
+++ server/server_test.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/inconshreveable/log15"
+	"github.com/msales/kage/testutil/mocks"
+)
+
+func TestServer_HealthPass(t *testing.T) {
+	svc := &mocks.MockService{Health: true}
+
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	srv := New("", []Service{svc}, log15.New())
+	srv.handleHealth(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestServer_HealthFail(t *testing.T) {
+	svc := &mocks.MockService{Health: false}
+
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	srv := New("", []Service{svc}, log15.New())
+	srv.handleHealth(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusInternalServerError)
+	}
+}
+
+func TestServer(t *testing.T) {
+	svc := &mocks.MockService{Health: true}
+
+	srv := New("127.0.0.1:0", []Service{svc}, log15.New())
+	srv.Start()
+	defer srv.Shutdown()
+
+	resp, err := http.Get("http://" + srv.ln.Addr().String() + "/health")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if status := resp.StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
