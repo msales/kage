c63be45617241974d1868fb577d1ea840ca6d78c
Nicholas Wiersma <nick@wiersma.co.za>
2017-05-23 10:39:41 +0200

    Test new application

---
+++ testutil/mocks/kafka.go
@@ -0,0 +1,19 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+// Kafka represents a mock Kafka client.
+type MockKafka struct {
+	mock.Mock
+}
+
+// IsHealthy checks the health of the Kafka client.
+func (m MockKafka) IsHealthy() bool {
+	args := m.Called()
+	return args.Bool(0)
+}
+
+// Close gracefully stops the Kafka client.
+func (m MockKafka) Close() {
+	m.Called()
+}
---
+++ testutil/mocks/store.go
@@ -0,0 +1,39 @@
+package mocks
+
+import (
+	"github.com/msales/kage"
+	"github.com/stretchr/testify/mock"
+)
+
+// Store represents a mock offset store.
+type MockStore struct {
+	mock.Mock
+}
+
+// AddOffset adds an offset into the store.
+func (m *MockStore) AddOffset(o *kage.PartitionOffset) {
+	m.Called(o)
+}
+
+// BrokerOffsets returns a snapshot of the current broker offsets.
+func (m *MockStore) BrokerOffsets() kage.BrokerOffsets {
+	args := m.Called()
+	return args.Get(0).(kage.BrokerOffsets)
+}
+
+// ConsumerOffsets returns a snapshot of the current consumer group offsets.
+func (m *MockStore) ConsumerOffsets() kage.ConsumerOffsets {
+	args := m.Called()
+	return args.Get(0).(kage.ConsumerOffsets)
+}
+
+// Channel get the offset channel.
+func (m *MockStore) Channel() chan *kage.PartitionOffset {
+	args := m.Called()
+	return args.Get(0).(chan *kage.PartitionOffset)
+}
+
+// Close gracefully stops the Store.
+func (m *MockStore) Close() {
+	m.Called()
+}
