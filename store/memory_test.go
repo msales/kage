cf547c671359637ef37ba3eb13f3ecd167405196
Nicholas Wiersma <nick@wiersma.co.za>
2017-09-19 07:31:12 +0200

    Add tests to check missing parition data in store

--- store/memory_test.go
+++ store/memory_test.go
@@ -59,6 +59,38 @@ func TestMemoryStore_BrokerOffsets(t *testing.T) {
 	assert.Equal(t, int64(1000), offsets["test"][0].NewestOffset)
 }
 
+func TestMemoryStore_BrokerOffsetsMissingParition(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionOffset{
+		Topic:               "test",
+		Partition:           1,
+		Oldest:              true,
+		Offset:              0,
+		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 2,
+	})
+	memStore.SetState(&store.BrokerPartitionOffset{
+		Topic:               "test",
+		Partition:           1,
+		Oldest:              false,
+		Offset:              1000,
+		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 2,
+	})
+
+	offsets := memStore.BrokerOffsets()
+
+	assert.Contains(t, offsets, "test")
+	assert.Len(t, offsets["test"], 2)
+	assert.Nil(t, offsets["test"][0])
+	assert.Equal(t, int64(0), offsets["test"][1].OldestOffset)
+	assert.Equal(t, int64(1000), offsets["test"][1].NewestOffset)
+}
+
 func TestMemoryStore_ConsumerOffsets(t *testing.T) {
 	memStore, err := store.New()
 	assert.NoError(t, err)
@@ -119,6 +151,37 @@ func TestMemoryStore_ConsumerOffsetsZeroOffset(t *testing.T) {
 	assert.Equal(t, int64(0), offsets["foo"]["test"][0].Lag)
 }
 
+func TestMemoryStore_ConsumerOffsetsMissingPartition(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionOffset{
+		Topic:               "test",
+		Partition:           1,
+		Oldest:              false,
+		Offset:              1000,
+		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 2,
+	})
+	memStore.SetState(&store.ConsumerPartitionOffset{
+		Group:     "foo",
+		Topic:     "test",
+		Partition: 1,
+		Offset:    0,
+		Timestamp: time.Now().Unix(),
+	})
+
+	offsets := memStore.ConsumerOffsets()
+
+	assert.Contains(t, offsets, "foo")
+	assert.Contains(t, offsets["foo"], "test")
+	assert.Len(t, offsets["foo"]["test"], 2)
+	assert.Nil(t, offsets["foo"]["test"][0])
+	assert.Equal(t, int64(0), offsets["foo"]["test"][1].Lag)
+}
+
 func TestMemoryStore_BrokerMetadata(t *testing.T) {
 	memStore, err := store.New()
 	assert.NoError(t, err)
@@ -144,6 +207,32 @@ func TestMemoryStore_BrokerMetadata(t *testing.T) {
 	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][0].Isr)
 }
 
+func TestMemoryStore_BrokerMetadataMissingPartition(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionMetadata{
+		Topic:               "test",
+		Partition:           1,
+		TopicPartitionCount: 2,
+		Leader:              100,
+		Replicas:            []int32{100, 101},
+		Isr:                 []int32{100, 101},
+		Timestamp:           time.Now().Unix(),
+	})
+
+	brokerMetadata := memStore.BrokerMetadata()
+
+	assert.Contains(t, brokerMetadata, "test")
+	assert.Len(t, brokerMetadata["test"], 2)
+	assert.Nil(t, brokerMetadata["test"][0])
+	assert.Equal(t, int32(100), brokerMetadata["test"][1].Leader)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][1].Replicas)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][1].Isr)
+}
+
 func TestMemoryStore_CleanConsumerOffsets(t *testing.T) {
 	memStore, err := store.New()
 	assert.NoError(t, err)
@@ -162,6 +251,33 @@ func TestMemoryStore_CleanConsumerOffsets(t *testing.T) {
 		Group:     "foo",
 		Topic:     "test",
 		Partition: 0,
+		Offset:    500,
+		Timestamp: time.Now().Unix()*1000 - (25 * int64(time.Hour.Seconds()) * 1000),
+	})
+
+	memStore.CleanConsumerOffsets()
+
+	assert.Len(t, memStore.ConsumerOffsets(), 0)
+}
+
+func TestMemoryStore_CleanConsumerOffsetsMissingPartition(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionOffset{
+		Topic:               "test",
+		Partition:           1,
+		Oldest:              false,
+		Offset:              1000,
+		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 2,
+	})
+	memStore.SetState(&store.ConsumerPartitionOffset{
+		Group:     "foo",
+		Topic:     "test",
+		Partition: 1,
 		Offset:    500,
 		Timestamp: time.Now().Unix()*1000 - (25 * int64(time.Hour.Seconds()) * 1000),
 	})
